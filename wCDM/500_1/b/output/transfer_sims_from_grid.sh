06af65d12c999c7bda6f5d9576a6dbfff81baef3
Joao <joao.reboucas1@gmail.com>
2023-06-28 16:19:06 -0300

    updating phase-reversed wCDM sims

---
+++ wCDM/500_1/b/output/transfer_sims_from_grid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in {0..60}; do
+    #if [ -d "./${i}" ]; then
+    #    echo Simulation ${i} is already here
+    #else
+        echo Downloading simulation ${i}...
+        scp -r joaoreboucas@access2.grid.unesp.br:~/COLA_projects/wCDM/outputs/${i}ph_rev . > /dev/null
+        if [ $? -ne 0 ]; then
+            echo Could not download simulation ${i} from Sdumont
+        fi
+    #fi
+done
